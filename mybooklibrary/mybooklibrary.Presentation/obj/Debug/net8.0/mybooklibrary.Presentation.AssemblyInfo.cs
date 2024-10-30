a4eda6fc9e3adb600d93d953fce88a50a77b1a8f
Serhat Sağlam <serhatsaglamb@gmail.com>
2024-10-30 10:59:09 +0300

    Views Imported

--- mybooklibrary/mybooklibrary.Presentation/obj/Debug/net8.0/mybooklibrary.Presentation.AssemblyInfo.cs
+++ mybooklibrary/mybooklibrary.Presentation/obj/Debug/net8.0/mybooklibrary.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mybooklibrary.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a00de3ae92b063981996f6ff850e2ff69a524f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217f7c8b8b6f1afcefa5fe29b8d5f7df1f1b18f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("mybooklibrary.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mybooklibrary.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
